98188c7bcbbb1732c5c944d317b64500d3e654e2
brunoaamello <brunoaamello@gmail.com>
2018-10-01 15:59:58 -0300

    Finished some funcitons, need to make a proper convergence checking function.

--- t1calcnum.c
+++ t1calcnum.c
@@ -8,6 +8,8 @@ typedef vector*   matrix;
 
 #define MAX_INT 0x7F
 
+#define SIG_DIGITS 7
+
 int initVector(vector* V, int size);
 int initMatrix(matrix* A, int size);
 int fillVectorRandomly(vector V, int maxVal, dataType divider, int size);
@@ -26,10 +28,10 @@ int main(){
 	int size, converges, maxIterations;
 	int eF = 0; //Error Flag
 	
-	size = 100;
+	size = 3;
 	
 	eF |= initMatrix(&A, size);
-	eF |= initMatrix(&coeficients, size);
+	eF |= initMatrix(&coeficients, size+1);
 	eF |= initVector(&B, size);
 	eF |= initVector(&(X[0]), size);
 	eF |= initVector(&(X[1]), size);
@@ -39,26 +41,28 @@ int main(){
 		exit(1);
 	}
 	
+	srand(time(NULL));
+	
 	do{
 		eF |= fillMatrixRandomly(A, MAX_INT, 384.23, size);
 		converges = checkConvergence(A, size);
 	}while(!converges && !eF);
 	
-	ef |= fillVectorRandomly(B, MAX_INT, 384.23, size);
+	eF |= fillVectorRandomly(B, MAX_INT, 384.23, size);
 	
-	if(ef){
+	if(eF){
 		printf("Error filling matrix A or vector B.\nExiting...\n");
 		exit(1);
 	}
 	
-	ef |= calculateCoeficients(A, B, coeficients, size);
+	eF |= calculateCoeficients(A, B, coeficients, size);
 	
 	if(eF){
 		printf("Error calculating coeficients.\nExiting...\n");
 		exit(1);
 	}
 	
-	maxIterations=2*size;
+	maxIterations=200*size;
 	
 	for(i=0;i<2*maxIterations && !eF;i++){
 		if(xOld==0){
@@ -120,10 +124,9 @@ int initMatrix(matrix* A, int size){
 
 int fillVectorRandomly(vector V, int maxVal, dataType divider, int size){
 	if(V && size>0){
-		srand(time(NULL));
 		int i;
 		for(i=0;i<size;i++){
-			V[i]=((dataType)(rand()%maxVal))/divider;
+			V[i]=(i+1)*((dataType)(rand()%maxVal))/divider;
 		}
 		return 0;
 	}else{
@@ -150,19 +153,78 @@ int checkConvergence(matrix A, int size){
 	if(!A || size<=0){
 		return 0;
 	}
-	
+	int i;
+	for(i=0;i<size;i++){
+		if(A[i][i]==0.0){
+			return 0;
+		}
+	}
+	return 1;
 }
 
 int calculateCoeficients(matrix A, vector B, matrix coeficients, int size){
-	
+	if(size<=0 || !(A && B && coeficients)){
+		return 1;
+	}
+	int i, j;
+	for(i=0;i<size;i++){
+		if(A[i][i]==0.0){
+			return 1;
+		}
+		for(j=0;j<size;j++){
+			if(i==j){
+				coeficients[i][j] = 0.0;
+			}else{
+				coeficients[i][j] = A[i][j]/A[i][i];
+			}
+		}
+		coeficients[i][size] = B[i]/A[i][i];
+	}
+	return 0;
 }
 
 int iterateGaussSeidel(vector oldX, vector newX, matrix coeficients, int size){
-	
+	if(size<=0 || !(oldX && newX && coeficients)){
+		return 1;
+	}
+	int i, j;
+	for(i=0;i<size;i++){
+		newX[i]=coeficients[i][size];
+		for(j=0;j<size;j++){
+			if(i<j){
+				newX[i]-=coeficients[i][j]*newX[j];
+			}else if(i>j){
+				newX[i]-=coeficients[i][j]*oldX[j];
+			}
+		}
+	}
+	return 0;
 }
 
 int printOperation(matrix A, vector X, vector B, int size){
-	
+	if(size<=0 || !(A && X && B)){
+		return 1;
+	}
+	int i, j;
+	for(i=0;i<size;i++){
+		printf("| ");
+		for(j=0;j<size;j++){
+			printf("%.*e ", SIG_DIGITS, A[i][j]);
+		}
+		if(i==size/2){
+			printf("| . ");
+		}else{
+			printf("|   ");
+		}
+		printf("| %.*e | ", SIG_DIGITS, X[i]);
+		if(i==size/2){
+			printf("= ");
+		}else{
+			printf("  ");
+		}
+		printf("| %.*e |\n", SIG_DIGITS, B[i]);
+	}
+	return 0;
 }
 
 //printf("%.*e\n", Digs, value);
